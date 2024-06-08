f8bb576d79e04267ea3e5ba81016a6201d11129f
Uri <uri555459@gmail.com>
2024-06-08 15:10:06 +0300

    Done

--- services/log.service.js
+++ services/log.service.js
@@ -1,15 +1,15 @@
-import chalk from 'chalk'
-import dedent from 'dedent-js'
+import chalk from 'chalk';
+import dedent from 'dedent-js';
 
-export const printError = error => {
-	console.log(`${chalk.bgRed(' ERROR ')} ${error}`)
-}
+const printError = (error) => {
+	console.log(chalk.bgRed(' ERROR ') + ' ' + error);
+};
 
-export const printSuccess = message => {
-	console.log(`${chalk.bgGreen(' SUCCESS ')} ${message}`)
-}
+const printSuccess = (message) => {
+	console.log(chalk.bgGreen(' SUCCESS ') + ' ' + message);
+};
 
-export const printHelp = () => {
+const printHelp = () => {
 	console.log(
 		dedent`${chalk.bgCyan(' HELP ')}
 		Без параметров - вывод погоды
@@ -17,5 +17,18 @@ export const printHelp = () => {
 		-h для вывода помощи
 		-t [API_KEY] для сохранения токена
 		`
-	)
-}
+	);
+};
+
+const printWeather = (res, icon) => {
+	console.log(
+		dedent`${chalk.bgYellow(' WEATHER ')} Погода в городе ${res.name}
+		${icon}  ${res.weather[0].description}
+		Температура: ${res.main.temp} (ощущается как ${res.main.feels_like})
+		Влажность: ${res.main.humidity}%
+		Скорость ветра: ${res.wind.speed}
+		`
+	);
+};
+
+export { printError, printSuccess, printHelp, printWeather };
