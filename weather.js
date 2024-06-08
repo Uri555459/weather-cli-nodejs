7bbc5f6a9c067d61db85774d99e239e548c72ec2
Uri <uri555459@gmail.com>
2024-06-08 11:15:00 +0300

    Added file service log.service.js

--- weather.js
+++ weather.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js'
+import { printHelp } from './services/log.service.js'
 
 const initCLI = () => {
 	const args = getArgs(process.argv)
-	console.log(args)
+
 	if (args.h) {
-		// show help
+		printHelp()
 	}
+
 	if (args.s) {
-		// save
+		// save city
 	}
+
 	if (args.t) {
 		// show weather
 	}
